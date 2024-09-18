02392cff6f46cb6af9d8d8ff534163dfa26b7466
Ander Ayesta <ander.ayesta@opendeusto.es>
2024-09-18 11:37:59 +0200

    Estructura 2

--- pruebagit/src/pruebagit/Principal.java
+++ pruebagit/src/pruebagit/Principal.java
@@ -3,7 +3,8 @@ package pruebagit;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo que tal");
+		System.out.println("Eeeeeeee");
 
 	}
 
