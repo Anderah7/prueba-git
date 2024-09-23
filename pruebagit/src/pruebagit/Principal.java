cef5bfdbe0551b115f68d5a7c46745622881c80d
Anderah7 <ander.ayesta@opendeusto.es>
2024-09-23 08:12:18 +0200

    Cambio del mensaje

--- pruebagit/src/pruebagit/Principal.java
+++ pruebagit/src/pruebagit/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo que tal");
-		System.out.println("Eeeeeeee");
+		System.out.println("otro");
 
 	}
 
