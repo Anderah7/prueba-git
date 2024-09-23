7156a533122e00babd8a37cd30182b1a2385fc37
Anderah7 <ander.ayesta@opendeusto.es>
2024-09-23 08:32:42 +0200

    cambio bucle

--- pruebagit/src/pruebagit/Principal.java
+++ pruebagit/src/pruebagit/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("aloo");
 		
-		for (int i = 2; i < args.length; i++) {
+		for (int i = 4; i < args.length; i++) {
 			System.out.println(i);
 		}
 
