bef01bbcfcc8fca74cb88b12c6c87670bcb8fef9
Ander Ayesta <ander.ayesta@opendeusto.es>
2024-09-23 08:30:02 +0200

    prueba cambio

--- pruebagit/src/pruebagit/Principal.java
+++ pruebagit/src/pruebagit/Principal.java
@@ -3,6 +3,11 @@ package pruebagit;
 public class Principal {
 
 	public static void main(String[] args) {
+		System.out.println("aloo");
+		
+		for (int i = 2; i < args.length; i++) {
+			System.out.println(i);
+		}
 
 	}
 
